02ca464c5f3b6e05aad36bd6ddfc441d58986fb6
SmaVivian <vivi163abc@163.com>
2018-12-13 12:05:24 +0800

    覆盖表单校验zh配置的消息和attributes

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -6,54 +6,26 @@ Vue.use(VeeValidate);
 
 Validator.localize('zh', zh);
 
+const dictionary = {
+  zh: {
+    messages: {
+      email: () => '请输入正确的邮箱格式',
+      required: ( field )=> "请输入" + field
+    },
+    attributes:{
+      email: '邮箱',
+      password:'密码',
+      name: '账号',
+      phone: '手机'
+    }
+  }
+};
+
+Validator.localize(dictionary);
+
 Validator.extend('phone', {
   getMessage: field => field + '必须是11位手机号码',
   validate: value => {
     return value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
   }
 });
-
-// let instance = new Validator({ trueField: 'phone' });
-
-// import Vue from 'vue'
-// import VeeValidate, {Validator} from 'vee-validate'
-// import zh from 'vee-validate/dist/locale/zh_CN'//引入中文文件
-
-// console.log(zh)
-// console.log(Validator.localize())
-
-// // 配置中文
-// Validator.localize(zh);
-
-// const config = {
-//   locale: 'zh_CN'
-// };
-
-// Vue.use(VeeValidate, config);
-
-// 自定义validate 
-// const dictionary = {
-//    zh_CN: {
-//       messages: {
-//         email: () => '请输入正确的邮箱格式',
-//         required: ( field )=> "请输入" + field
-//       },
-//       attributes:{
-//         email:'邮箱',
-//         password:'密码',
-//         name: '账号',
-//         phone: '手机'
-//       }
-//   }
-// };
-
-// Validator.updateDictionary(dictionary);
-
-// Validator.extend('phone', {
-//   messages: {
-//     zh_CN:field => field + '必须是11位手机号码',
-//   },
-//   validate: value => {
-//     return value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
-//   }
-// });
